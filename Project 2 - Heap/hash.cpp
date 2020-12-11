37f231a5c3a41ca11ff7e8006a61c5df29959001
Mark Koszykowski <31666939+markkoszykowski@users.noreply.github.com>
2020-12-10 22:16:48 -0500

    Add files via upload

---
+++ Project 2 - Heap/hash.cpp
@@ -0,0 +1,150 @@
+/*
+        Mark Koszykowski
+        ECE365 - DSA II
+        Programming Assignment 2
+*/
+
+#include "hash.h"
+#include <iostream>
+
+using namespace std;
+
+hashTable::hashTable(int size): capacity(getPrime(size)), filled(0), data(capacity) {}
+
+int hashTable::insert(const string &key, void *pv) {
+        int val = findPos(key);
+
+        if(val == -1) {
+                val = hash(key);
+
+                while(data[val].isOccupied) {
+                        if(data[val].key == key) {
+                                break;
+                        }
+                        ++val %= capacity;
+                }
+
+                data[val].key = key;
+                data[val].isOccupied = true;
+                data[val].isDeleted = false;
+                data[val].pv = pv;
+                
+                if((++filled) * 2 > capacity) {
+                        if(!rehash()) {
+                                return 2;
+                        }
+                }
+                return 0;
+        }
+        else {
+                return 1;
+        }
+}
+
+bool hashTable::contains(const string &key) {
+        return (findPos(key) != -1);
+}
+
+void *hashTable::getPointer(const string &key, bool *b) {
+        int val = findPos(key);
+
+        if(val != -1) {
+                *b = true;
+                return data[val].pv;
+        }
+        else {
+                *b = false;
+                return nullptr;
+        }
+}
+
+int hashTable::setPointer(const string &key, void *pv) {
+        int val = findPos(key);
+
+        if(val != -1) {
+                data[val].pv = pv;
+                return 0;
+        }
+        else {
+                return 1;
+        }
+}
+
+bool hashTable::remove(const string &key) {
+        int val = findPos(key);
+
+        if(val != -1) {
+                data[val].isDeleted = true;
+                return true;
+        }
+        else {
+                return false;
+        }
+}
+
+
+// Source: https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+// Chose algorithm with lowest times and collisions
+int hashTable::hash(const string &key) {
+        unsigned int hash = 0;
+        unsigned int len = key.length();
+
+        for(int i = 0; i < len; i++) {
+                hash = key[i] + (hash << 6) + (hash << 16) - hash;
+        }
+
+        return hash % capacity;
+}
+
+int hashTable::findPos(const string &key) {
+        int val = hash(key);
+
+        while(data[val].isOccupied) {
+                if(data[val].key == key) {
+                        if(data[val].isDeleted) {
+                                return -1;
+                        }
+                        else {
+                                return val;
+                        }
+                }
+
+                ++val %= capacity;
+        }
+
+        return -1;
+}
+
+bool hashTable::rehash() {
+        vector<hashItem> old = data;
+
+        data.clear();
+        capacity = getPrime(capacity);
+        data.resize(capacity);
+        if(data.size() != capacity) {
+                return false;
+        }
+
+        filled = 0;
+        for(int i = 0; i < old.size(); i++) {
+                hashItem oldItem = old[i];
+                if(old[i].isOccupied && !(old[i].isDeleted)) {
+                        insert(oldItem.key, oldItem.pv);
+                }
+        }
+
+        return true;
+}
+
+unsigned int hashTable::getPrime(int size) {
+        unsigned int primes[] = {100003, 200003, 400009, 800011, 1600033, 3200003, 6400013};
+        unsigned int len = (sizeof(primes) / sizeof(int));
+
+        for(int i = 0; i < len; i++) {
+                if(primes[i] > (2 * size)) {
+                        return primes[i];
+                }
+        }
+
+        return primes[len - 1];
+}
---
+++ Project 2 - Heap/heap.cpp
@@ -0,0 +1,153 @@
+/*
+        Mark Koszykowski
+        ECE365 - DSA II
+        Programming Assignment 2
+*/
+
+#include "heap.h"
+#include <iostream>
+
+using namespace std;
+
+heap::heap(int cap) {
+        capacity = cap;
+        size = 0;
+        data.resize(capacity + 1);
+        myHash = new hashTable(capacity * 2);
+}
+
+int heap::insert(const string &id, int key, void *pv) {
+        if(size >= capacity) {
+                return 1;
+        }
+        else if(myHash->contains(id)) {
+                return 2;
+        }
+        else {
+                size += 1;
+                data[size].id = id;
+                data[size].key = key;
+                data[size].ptr = pv;
+                myHash->insert(id, &data[size]);
+                percolateUp(size);
+
+                return 0;
+        }
+}
+
+int heap::setKey(const string &id, int key) {
+        bool b;
+        node *temp = static_cast<node *>(myHash->getPointer(id, &b));
+        if(b == false) {
+                return 1;
+        }
+        else {
+                int pos, oldKey;
+
+                oldKey = temp->key;
+                temp->key = key;
+                pos = findPos(temp);
+
+                if(oldKey > key) {
+                        percolateUp(pos);
+                }
+                else if(oldKey < key) {
+                        percolateDown(pos);
+                }
+                return 0;
+        }
+}
+
+int heap::deleteMin(string *pId, int *pKey, void *ppData) {
+        if(size == 0) {
+                return 1;
+        }
+        else {
+                if(pId) {
+                        *pId = data[1].id;
+                }
+                if(pKey) {
+                        *pKey = data[1].key;
+                }
+                if(ppData) {
+                        *(static_cast<void **>(ppData)) = data[1].ptr;
+                }
+                myHash->remove(data[1].id);
+                data[1] = data[size];
+                size -= 1;
+                percolateDown(1);
+                return 0;
+        }
+}
+
+int heap::remove(const string &id, int *pKey, void *ppData) {
+        bool b;
+        node *temp = static_cast<node *>(myHash->getPointer(id, &b));
+        if(b == false) {
+                return 1;
+        }
+        else {
+                int pos, oldKey;
+
+                if(pKey) {
+                        *pKey = temp->key;
+                }
+                if(ppData) {
+                        *(static_cast<void **>(ppData)) = temp->ptr;
+                }
+                
+                myHash->remove(id);
+                oldKey = temp->key;
+                *temp = data[size];
+                size -= 1;
+                pos = findPos(temp);
+                
+                if(oldKey > temp->key) {
+                        percolateUp(pos);
+                }
+                else if(oldKey < temp->key) {
+                        percolateDown(pos);
+                }
+                return 0;
+        }
+}
+
+void heap::percolateUp(int pos) {
+        node inserted = data[pos];
+        while(pos > 1) {
+                if(inserted.key < data[pos/2].key) {
+                        data[pos] = data[pos/2];
+                        myHash->setPointer(data[pos].id, &data[pos]);
+                        pos /= 2;
+                }
+                else {
+                        break;
+                }
+        }
+        data[pos] = inserted;
+        myHash->setPointer(data[pos].id, &data[pos]);
+}
+
+void heap::percolateDown(int pos) {
+        int child;
+        node removed = data[pos];
+        while((child = pos * 2) <= size) {
+                if(child < size && data[child].key > data[child + 1].key) {
+                        child += 1;
+                }
+                if(removed.key > data[child].key) {
+                        data[pos] = data[child];
+                        myHash->setPointer(data[pos].id, &data[pos]);
+                        pos = child;
+                }
+                else {
+                        break;
+                }
+        }
+        data[pos] = removed;
+        myHash->setPointer(data[pos].id, &data[pos]);
+}
+
+int heap::findPos(node *ptr) {
+        return (ptr - &data[0]);
+}
